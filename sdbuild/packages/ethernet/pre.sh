4951184fd2e6ecebbb3bd4c699f67a8dbfbc2ae8
Cameron Dykstra <cameron@resonint.com>
2022-07-13 16:28:53 +1200

    use systemd-networkd for ethernet configuration

--- sdbuild/packages/ethernet/pre.sh
+++ sdbuild/packages/ethernet/pre.sh
@@ -6,4 +6,4 @@ set -e
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo cp $script_dir/eth0 $target/etc/network/interfaces.d
+sudo cp $script_dir/eth0.network $target/etc/systemd/network/
