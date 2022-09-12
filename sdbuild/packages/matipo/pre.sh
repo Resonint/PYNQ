fe2c402fe742802e5078e424996b187b7fd2f071
Cameron Dykstra <dykstra.cameron@gmail.com>
2022-09-12 16:25:47 +1200

    add docker compose file and service to autorun it, remove unneeded python module installs.

--- sdbuild/packages/matipo/pre.sh
+++ sdbuild/packages/matipo/pre.sh
@@ -9,6 +9,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $script_dir/system/ $target/home/xilinx/
 
+sudo mkdir -p $target/etc/matipo/
+sudo cp $script_dir/docker-compose.yml $target/etc/matipo/
+sudo cp $script_dir/matipo.service $target/etc/systemd/system/
 if [ -d "$script_dir/pre-built/$target_dir" ]
 then
     sudo mkdir -p $target/$target_dir
--- sdbuild/packages/matipo/qemu.sh
+++ sdbuild/packages/matipo/qemu.sh
@@ -6,25 +6,10 @@ export HOME=/root
 set -e
 set -x
 
-. /etc/environment
-for f in /etc/profile.d/*.sh; do source $f; done
+pynq_hostname.sh ilumr
 
-# required for building jupyterlab with limited RAM
-export NODE_OPTIONS=--max-old-space-size=2048
-
-# TODO: replace with running an install.sh file
-cd /home/xilinx/system/
-python3 -m pip install ./matipo-python
-
-cd /home/xilinx/system/
-python3 -m pip install ./jupyterlab-panel-server
-
-cd /home/xilinx/system/
-python3 -m pip install ./jupyterlab-time-sync
-
-cp /home/xilinx/system/services/*.service /etc/systemd/system/
-sudo systemctl enable matipo-daemon
-sudo systemctl enable matipo-jupyter
+# TODO: enable service to run docker container(s)
+systemctl enable matipo
 
 echo "xilinx ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 # fix, for some reason /etc/sudoers.d/README get wrong owner
